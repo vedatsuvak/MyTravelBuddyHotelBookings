c51059f63658e3a892a506f472aa2babe4887cd0
vedatsuvak <vedatsuvak@icloud.com>
2024-07-20 19:37:25 +0100

    MyTravelBuddy Hotels

--- src/test/java/com/example/demo/MyTravelBuddyHotelBookingsApplicationTests.java
+++ src/test/java/com/example/demo/MyTravelBuddyHotelBookingsApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+// @SpringBootTest
 class MyTravelBuddyHotelBookingsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
